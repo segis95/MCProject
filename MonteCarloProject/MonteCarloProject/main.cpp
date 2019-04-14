836746709d83b56afb15692d5f7d912fd7137046
segis95 <segis95@mail.ru>
2019-04-14 19:36:46 +0300

    final commit
    final commit

--- MonteCarloProject/MonteCarloProject/main.cpp
+++ MonteCarloProject/MonteCarloProject/main.cpp
@@ -16,71 +16,98 @@ int main()
 	generator.seed(seed);
 	std::clock_t start;
 	double duration;
-
-	/*
-
-
-	int num_sim = 1000000;
+	double s, mean, var, sim, confidence;
+	
+	int num_sim = 1000000;//number of simulations - change here to decrease the execution time
 
 	double steps[] = { 0.05, 0.1, 0.125, 0.2, 0.25 };
-
-	std::cout << "Number of simulations for caplets: " << num_sim << "\n";
-	std::cout << "******ALGORITHM 1********\n";
-	for (int j = 0; j < 5; j++) {
-		start = std::clock();
-		double s = 0.0;
-		for (int i = 0; i < num_sim; i++) {
-			s += one_simulation(0.13, 0.01, 0.28, 0.25, steps[j], 9, 1);
-		}
-		duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-		std::cout << "step: " << steps[j] << ", result: " << s / num_sim << ", duration: " << duration << "s\n";
-	}
-
-	std::cout << "******ALGORITHM 2********\n";
-	for (int j = 0; j < 5; j++) {
-		start = std::clock();
-		double s = 0.0;
-		for (int i = 0; i < num_sim; i++) {
-			s += one_simulation(0.13, 0.01, 0.28, 0.25, steps[j], 9, 2);
-		}
-		duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-		std::cout << "step: " << steps[j] << ", result: " << s / num_sim << ", duration: " << duration << "s\n";
-	}
-
-	*/
-	//std::cout << classical_method(5, 0.13, 0.01, 0.28, 0.25, 0.05, 9, 1);
-
-	std::cout << "******CLASSICAL METHODS********\n";
+	std::cout << "Attention! Execution of the entire of this code may take upto 30 minutes! \n";
+	std::cout << "if you want to decrease the execution time please change the values of parameters num_sim and N\n\n";
+	std::cout << "******RANDOM WALK APPROACH********\n\n";
 	
-	double s = 0.0;
-	int N = 10000; // number of simulations
-	double c;
-	int deg = 14; // 2^deg + 1 point for brownian motion
+	std::cout << "Number of simulations for caplets: " << num_sim << "\n\n";
+	std::cout << "******ALGORITHM 1********\n";
+	var = 0.0;
+	for (int j = 0; j < 5; j++) {
+		start = std::clock();
+		s = 0.0;
+		for (int i = 0; i < num_sim; i++) {
+			sim = one_simulation(0.13, 0.01, 0.28, 0.25, steps[j], 9, 1);
+			s += sim;
+			var += sim * sim;
+		}
+		mean = s / num_sim;
+		var = (var / num_sim - mean * mean);
+		confidence = 1.96 * sqrt(var/num_sim);
+
+		duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
+		std::cout << "step: " << steps[j] << ", result: " << mean << ", IC(+-): "<< confidence << "duration: " << duration << "s\n";
+	}
+	std::cout << "\n\n";
+	std::cout << "******ALGORITHM 2********\n";
+	var = 0.0;
+	for (int j = 0; j < 5; j++) {
+		start = std::clock();
+		s = 0.0;
+		for (int i = 0; i < num_sim; i++) {
+			sim = one_simulation(0.13, 0.01, 0.28, 0.25, steps[j], 9, 2);
+			s += sim;
+			var += sim * sim;
+		}
+
+		mean = s / num_sim;
+		var = (var / num_sim - mean * mean);
+		confidence = 1.96 * sqrt(var / num_sim);
+
+		duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
+		std::cout << "step: " << steps[j] << ", result: " << mean << ", IC(+-): " << confidence << ", duration: " << duration << "s\n";
+	}
+
+	
+	std::cout << "\n\n\n\n";
+	std::cout << "******CLASSICAL METHODS********\n\n";
+	
+	int N = 10000; // number of simulations - change here to decrease the execution time
+	int deg = 15; // 2^deg + 1 points for brownian motion
 	//*
 	start = std::clock();
 
-	std::cout << "******Number of simulations: " << N << "*****\n";
-	std::cout << "******Number of points for brownian: " << deg << "*****\n";
-	std::cout << "****CLASSICAL METHOD WITH PONDERATION****\n";
+	std::cout << "******Number of simulations: " << N << "*****\n\n";
+	std::cout << "******Number of points for brownian: 2^" << deg << "*****\n\n";
+	std::cout << "******CLASSICAL METHOD WITH PONDERATION****\n";
 
+	var = 0.0;
+	s = 0.0;
 	for (int i = 0; i < N; i++) {
-		c = classical_method(deg, 0.13, 0.01, 0.28, 0.25, 0.01, 9, 0);
-		s += c ;
+		sim = classical_method(deg, 0.13, 0.01, 0.28, 0.25, 0.01, 9, 0);
+		s += sim ;
+		var += sim * sim;
 	}
 	
+	mean = s / N;
+	var = (var / N - mean * mean);
+	confidence = 1.96 * sqrt(var / N);
+
 	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-	std::cout << "result: " << s / N << '\n';
+	std::cout << "result: " << mean << ", IC(+-): " << confidence << '\n';
 	std::cout << "time elapsed " << duration << "s\n\n";
 
 	std::cout << "CLASSICAL METHOD WITHOUT PONDERATION\n";
 	start = std::clock();
 	s = 0.0;
+	var = 0.0;
 	for (int i = 0; i < N; i++) {
-		c = classical_method(deg, 0.13, 0.01, 0.28, 0.25, 0.01, 9, 1);
-		s += c;
+		sim = classical_method(deg, 0.13, 0.01, 0.28, 0.25, 0.01, 9, 1);
+		s += sim;
+		var += sim * sim;
 	}
+
+	mean = s / N;
+	var = (var / N - mean * mean);
+	confidence = 1.96 * sqrt(var / N);
+
 	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-	std::cout << "result: " << s / N << '\n';
+	std::cout << "result: " << mean <<", IC(+-): "<< confidence <<'\n';
 	std::cout << "time elapsed " << duration << "s\n\n";
 	
 
--- MonteCarloProject/MonteCarloProject/utils.cpp
+++ MonteCarloProject/MonteCarloProject/utils.cpp
@@ -6,27 +6,35 @@
 #include "utils.h"
 
 
-
+//d_+ function Black-Scholes
 double d_p(double x, double v) {
 	return (log(x) + v * v / 2) / v;
 }
 
+//d_- function Black-Scholes
 double d_m(double x, double v) {
 	return (log(x) - v * v / 2) / v;
 }
 
+// cumulative distribution function
+// of normal distribution N(0,1)
 double cdf(double value) {
 	return 0.5 * erfc(-value * M_SQRT1_2);
 }
 
+// probability distribution function
+// of normal distribution N(0,1)
 double pdf(double x) {
 	return exp(-x * x / 2) / sqrt(2 * PI);
 }
 
+// a technical redefinition
 double d_prime(double x, double v) {
 	return 1.0 / (x * v);
 }
 
+// derivative of the ground truth option price
+// approved with a numerical derivation
 double dF_dL(double L_i, double K, double H, double v_i) {
 	return cdf(d_p(L_i / K, v_i)) - cdf(d_p(L_i / H, v_i)) + \
 		L_i * (pdf(d_p(L_i / K, v_i)) * d_prime(L_i / K, v_i) / K - \
@@ -41,6 +49,7 @@ double dF_dL(double L_i, double K, double H, double v_i) {
 
 }
 
+// ground truth option price
 double libor_price(double L_i, double K, double H, double v_i) {
 	return L_i * (cdf(d_p(L_i / K, v_i)) - cdf(d_p(L_i / H, v_i))) - \
 		K * (cdf(d_m(L_i / K, v_i)) - cdf(d_m(L_i / H, v_i))) - \
@@ -48,16 +57,18 @@ double libor_price(double L_i, double K, double H, double v_i) {
 		K * L_i / H * (cdf(d_m(H * H / K / L_i, v_i)) - cdf(d_m(H / L_i, v_i)));
 }
 
-
+// bernully variable generator
 double bern(double p) {
 	return distribution(generator) < p ? -1.0 : 1.0;
 }
 
+//  positive part function
 double pos_part(double x) {
 	return x > 0.0 ? x : 0.0;
 }
 
-
+// one simulation of the algorithm I(if mode = 1) or 
+// algorithm II(if mode = 2)
 double one_simulation(double L_i, double K, double H, double sigma, double h, double i, int mode) {
 
 	int M = int(i / h);
@@ -95,10 +106,17 @@ double one_simulation(double L_i, double K, double H, double sigma, double h, do
 	return pos_part(exp(lnL) - K) + Z;
 
 }
+
+// lognormal process prototype
 double logn(double x, double sigma, double t) {
 	return exp(-sigma * sigma * t / 2.0 + sigma * x);
 }
 
+
+// one simulation of the classical method
+// the brownian motion is generated via brownian bridge
+// mode = 0 corresponds to the classical brownian bridge MC approach
+// mode = 1 corresponds to the case without ponderation
 double classical_method(int N, double L_i, double K, double H, double sigma, double h, double i, int mode) {
 	
 	int lgth = pow(2, N);
@@ -114,18 +132,17 @@ double classical_method(int N, double L_i, double K, double H, double sigma, dou
 	double Z;
 	int level = lgth;
 
+	//brownian bridge
 	for (int j = 1; j < N + 1; j++) {
 		level /= 2;
 		step = level * 2;
 		for (k = level; k < lgth; k = k + step) {
 			Z = gauss(generator) * (sqrt(double(level) / lgth / 2.0) * tc);
 			W[k] = (W[k - level] + W[k + level]) / 2.0 + Z;
-			//std::cout << Z << ' ' << W[k - level] << ' ' << W[k] <<' '<<W[k + level] << '\n';
 
 		}
 	}
 
-	//return W[1];
 	double max = 0.0;
 	double delta = 1.0 / lgth; 
 	
@@ -135,15 +152,13 @@ double classical_method(int N, double L_i, double K, double H, double sigma, dou
 		
 		if (W[j] > max) {
 			max = W[j];
-			//std::cout << max << '\n'; 
 		}
 		if (j < lgth) {
 			multiplier *= (1.0 - exp(-2.0 * double(lgth) / (i * sigma * sigma) * \
 				(W[j] - H) * (W[j + 1] - H))) ;
 		}
 	}
-	//return W[lgth];
-	//std::cout << multiplier << '\n';
+
 	if (max < H) {
 		if (mode == 0)
 			return W[lgth] - K > 0.0 ? (W[lgth] - K) * multiplier : 0.0;
@@ -151,6 +166,5 @@ double classical_method(int N, double L_i, double K, double H, double sigma, dou
 			return W[lgth] - K > 0.0 ? (W[lgth] - K) : 0.0;
 	}
 	else
-		//std::cout << 1;
 		return 0.0;
 }
